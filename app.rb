f0b83a7ba3f198af3731b44f82998d605d155009
Christopher Wood <cjwood032@gmail.com>
2018-07-22 15:46:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! We've got fun and games! "
+    "Welcome to your app!!!! We've got fun and games! We've got everything you'd want."
   end
 
 end
